5dcb3334735598be395b0a2fcff6594f1de8d6a6
levalyukov <lev.alyukov@gmail.com>
2025-06-06 11:06:44 +0500

    Update player.h

--- src/entities/player.h
+++ src/entities/player.h
@@ -11,25 +11,25 @@ const float WALK_SPEED = 4.0f;
 const int RUN_SPEED = 6.0f;
 
 class Player {
-public:
-	Player();
-	void render(sf::RenderWindow& window, float& deltaTime);
-	void movement(float& deltaTime);
+	public:
+		Player();
+		void render(sf::RenderWindow& window, float& deltaTime);
+		void movement(float& deltaTime);
 
-private:
-	Animations animations;
+	private:
+		Animations animations;
 
-	sf::Sprite sprite;
-	sf::Texture texture_walk_vertical;
-	sf::Texture texture_walk_horizontal;
-	sf::Texture texture_idle;
-	sf::View view;
+		sf::Sprite sprite;
+		sf::Texture texture_walk_vertical;
+		sf::Texture texture_walk_horizontal;
+		sf::Texture texture_idle;
+		sf::View view;
 
-	std::string direction;
-	sf::Vector2i animWalk_UP = { 0,16 };
-	sf::Vector2i animWalk_DOWN = { 0,0 };
-	sf::Vector2i animWalk_LEFT = { 0,0 };
-	sf::Vector2i animWalk_RIGHT = { 0,16 };
+		std::string direction;
+		sf::Vector2i animWalk_UP = { 0,16 };
+		sf::Vector2i animWalk_DOWN = { 0,0 };
+		sf::Vector2i animWalk_LEFT = { 0,0 };
+		sf::Vector2i animWalk_RIGHT = { 0,16 };
 
-	void camera(sf::RenderWindow& window);
+		void camera(sf::RenderWindow& window);
 };

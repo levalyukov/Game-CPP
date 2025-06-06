31e1070adab7cdfe9c5c019759d56436f6363367
levalyukov <lev.alyukov@gmail.com>
2025-06-06 11:06:46 +0500

    Update tilemap.h

--- src/systems/tilemap/tilemap.h
+++ src/systems/tilemap/tilemap.h
@@ -6,18 +6,18 @@
 #include <SFML/Graphics/Color.hpp>
 
 class Tilemap {
-public:
-	Tilemap();
-	void render(sf::RenderWindow&);
+	public:
+		Tilemap();
+		void render(sf::RenderWindow&);
 
-private:
-	const int TILE_SIZE = 16 * 4;
-	const int WIDHT_MAX = 32;
-	const int HEIGH_MAX = 32;
+	private:
+		const int TILE_SIZE = 16 * 4;
+		const int WIDHT_MAX = 32;
+		const int HEIGH_MAX = 32;
 
-	sf::Texture grass_texture;
-	sf::Sprite grass_sprite;
+		sf::Texture grass_texture;
+		sf::Sprite grass_sprite;
 
-	void initilize();
+		void initilize();
 };
 

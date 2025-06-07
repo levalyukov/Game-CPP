2777b59ce9482507e5712f8ffcd61ed10c747f20
levalyukov <lev.alyukov@gmail.com>
2025-06-07 08:09:32 +0500

    Create ui.cpp

---
+++ src/ui/ui.cpp
@@ -0,0 +1,20 @@
+#include "ui.h"
+
+UI::UI() {
+	initilize(font);
+}
+
+void UI::render(sf::RenderWindow& window) {
+	window.draw(test_text);
+}
+
+void UI::initilize(sf::Font& font) {
+	if (!font.loadFromFile("../../../assets/fonts/arial.ttf")) {
+		std::cout << "Error: 'arial.ttf' not loaded." << std::endl;
+		return;
+	}
+	test_text.setPosition(10.f, 10.f);
+	test_text.setString("Hello, Gamedev");
+	test_text.setCharacterSize(50);
+	test_text.setFont(font);
+}

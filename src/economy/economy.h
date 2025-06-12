ec20aca0c1f52314fcd1564933457de4f2cc2489
levalyukov <lev.alyukov@gmail.com>
2025-06-12 22:29:28 +0500

    Create economy.h

---
+++ src/economy/economy.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+
+class Economy {
+	public:
+		int money = 0;
+
+		void setMoney(int value);
+		void addMoney(int value);
+		void removeMoney(int value);
+
+	private: 
+		
+};

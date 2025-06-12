b2ce010010df1577e8bcd4fb6e8025c9f36d33b8
levalyukov <lev.alyukov@gmail.com>
2025-06-12 22:29:30 +0500

    Create economy.cpp

---
+++ src/economy/economy.cpp
@@ -0,0 +1,28 @@
+#include "economy.h";
+
+void Economy::setMoney(int value) {
+	if (value < 0) {
+		money = 0;
+		return;
+	}
+
+	money = value;
+}
+
+void Economy::addMoney(int value) {
+	if (value < 0) {
+		std::cerr << "Error: Value not can't be less zero." << std::endl;
+		return;
+	}
+	
+	money += value;
+}
+
+void Economy::removeMoney(int value) {
+	if (value < 0) {
+		std::cerr << "Error: Value not can't be less zero." << std::endl;
+		return;
+	}
+
+	money -= value;
+}

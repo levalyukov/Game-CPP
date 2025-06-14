db0a4c6742ec2f2554c8ac5d9836016c837cc530
levalyukov <lev.alyukov@gmail.com>
2025-06-14 17:56:47 +0500

    update tilemap

--- src/systems/tilemap/tilemap.cpp
+++ src/systems/tilemap/tilemap.cpp
@@ -5,16 +5,37 @@ Tilemap::Tilemap() {
 }
 
 void Tilemap::initilize() {
-	if (!grass_texture.loadFromFile("../../../assets/Textures/World/grass.png")) return;
+	if (!rock_texture.loadFromFile("../../../assets/textures/location/tiles/rock.png")) return;
+	if (!water_texture.loadFromFile("../../../assets/textures/location/tiles/water.png")) return;
+	if (!grass_texture.loadFromFile("../../../assets/textures/location/tiles/grass.png")) return;
 
+	rock_sprite.setTexture(rock_texture);
+	water_sprite.setTexture(water_texture);
 	grass_sprite.setTexture(grass_texture);
+
+	rock_sprite.setScale(4, 4);
+	water_sprite.setScale(4, 4);
 	grass_sprite.setScale(4, 4);
 }
 
 void Tilemap::render(sf::RenderWindow& window) {
 	for (int x = 0; x < 9; x++) {
 		for (int y = 0; y < 9; y++) {
-			grass_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+
+			if (x % 9 == 0) water_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+			else if (y % 9 == 0) water_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+			else if (x % 9 == 8) water_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+			else if (y % 9 == 8) water_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+
+			else if (x % 9 == 2 && y >= 2 && y <= 6) rock_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+			else if (x % 9 == 6 && y >= 2 && y <= 6) rock_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+			else if (y % 9 == 2 && x >= 2 && x <= 6) rock_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+			else if (y % 9 == 6 && x >= 2 && x <= 6) rock_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+
+			else grass_sprite.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+
+			window.draw(rock_sprite);
+			window.draw(water_sprite);
 			window.draw(grass_sprite);
 		}
 	}
--- src/systems/tilemap/tilemap.h
+++ src/systems/tilemap/tilemap.h
@@ -15,7 +15,11 @@ class Tilemap {
 		const int WIDHT_MAX = 32;
 		const int HEIGH_MAX = 32;
 
+		sf::Texture rock_texture;
+		sf::Texture water_texture;
 		sf::Texture grass_texture;
+		sf::Sprite rock_sprite;
+		sf::Sprite water_sprite;
 		sf::Sprite grass_sprite;
 
 		void initilize();

0a9b3da7a1143767232bc3b5386f0a54f2f32481
levalyukov <lev.alyukov@gmail.com>
2025-07-11 11:07:34 +0500

    include logger in game.hpp

--- src/core/game.hpp
+++ src/core/game.hpp
@@ -2,6 +2,7 @@
 
 #include "tilemap/tilemap.hpp"
 #include "resources/resources.hpp"
+#include "debug/logger.hpp"
 
 #include "../entities/entity-manager.hpp"
 #include "../ui/core/ui.hpp"
@@ -10,6 +11,8 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Config.hpp>
+#include <SFML/System/Err.hpp>
 
 class Game {
 	public:
@@ -32,15 +35,17 @@ class Game {
 		void render();
 		void initilizeIcon();
 
+		Logger& logger = Logger::Instance();
 		ResourceManager& resourceManager = ResourceManager::Instance();
 		UIManager& UIManager = UIManager::Instance();
+
 		Tilemap tilemap;
 		EntityManager entityManager;
 		HUD hud;
 
 		sf::Image icon;
 		const sf::Uint8* iconData;
-
+		
 		float rawDelta = clock.restart().asSeconds();
 		float deltaTime = std::min(rawDelta, 0.1f);
 };
